0c258f7d53d1e1bb1a3b6c85c6c38d2af0032c9a
annaAzh <forarre@yandex.ru>
2024-10-01 16:49:01 +0300

    feat: impelent copy fs task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
+import {cp, access, constants} from 'fs/promises';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
+const isExistDirectory = async (path) => {
+    return await access(path, constants.R_OK).then(() => true).catch(() => false);
+}
+
 const copy = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const __dirname = dirname(fileName);
+
+    try {
+        const pathFrom = __dirname + '/files';
+        const pathTo = __dirname + '/files_copy';
+        const errorMessage = 'FS operation failed';
+
+        const isExistsFrom = await isExistDirectory(pathFrom);
+        const isExistsTo = await isExistDirectory(pathTo);
+
+        if (!isExistsFrom || isExistsTo) {
+            throw new Error(errorMessage);
+        } else {
+            cp(pathFrom, pathTo, {recursive: true});
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
 };
 
 await copy();
