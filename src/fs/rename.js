3db942a3c69feb9c3c019110a2b37125b73ffbb2
annaAzh <forarre@yandex.ru>
2024-10-01 17:10:09 +0300

    feat: implement rename fs task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import {rename as fsRename} from 'fs/promises';
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const __dirname = dirname(fileName);
+
+    try {
+        const pathToFile = __dirname + '/files/wrongFilename.txt';
+        const pathToRename = __dirname + '/files/properFilename.md';
+        const errorMessage = 'FS operation failed';
+
+        await fsRename(pathToFile, pathToRename).catch(() => {
+            throw new Error(errorMessage);
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
 };
 
 await rename();
