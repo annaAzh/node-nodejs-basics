c6eb5dc17addb18b14de9179fd2fcabec55e7265
annaAzh <forarre@yandex.ru>
2024-10-03 22:09:01 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import {createHash} from 'crypto';
+import {createReadStream} from 'fs';
+import {fileURLToPath} from 'url';
+import {dirname, join} from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const fileName = fileURLToPath(import.meta.url);
+    const __dirname = dirname(fileName);
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', (data) => {
+        hash.update(data);
+    });
+    
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();
